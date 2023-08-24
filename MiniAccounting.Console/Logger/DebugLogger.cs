5fbd2711f9ece24b13939b5d9e0e22bda42878b6
DaHu26 <127865164+DaHu26@users.noreply.github.com>
2023-08-24 19:19:27 +0300

    Реализовал логгирование (#2)

---
+++ MiniAccounting.Console/Logger/DebugLogger.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace MiniAccountingConsole.Logger
+{
+    public sealed class DebugLogger : AbstractLogger
+    {
+        public static readonly DebugLogger Instance = new DebugLogger();
+
+        protected override void PrivateWrite(string fullMsg)
+        {
+            Debug.WriteLine(fullMsg);
+        }
+    }
+}
