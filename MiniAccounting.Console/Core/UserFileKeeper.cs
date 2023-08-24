793da9996222642f8e115ad3ee282e0fedff4d2c
DaHu <plis-danil@mail.ru>
2023-08-24 16:21:35 +0300

    Начал делать

---
+++ MiniAccounting.Console/Core/UserFileKeeper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAccountingConsole.Core
+{
+    internal class UserFileKeeper : IUserKeeper
+    {
+        public void Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User ReadUser(Guid uid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User[] ReadUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
