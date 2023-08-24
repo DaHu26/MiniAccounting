f4a250849e359229d89c955986f678b957b80cbf
DaHu <plis-danil@mail.ru>
2023-08-24 16:23:28 +0300

    .

--- MiniAccounting.Console/Core/UserFileKeeper.cs
+++ MiniAccounting.Console/Core/UserFileKeeper.cs
@@ -10,27 +10,27 @@ namespace MiniAccountingConsole.Core
     {
         public void Delete(User user)
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Edit(User user)
         {
-            throw new NotImplementedException();
+            
         }
 
         public User ReadUser(Guid uid)
         {
-            throw new NotImplementedException();
+            
         }
 
         public User[] ReadUsers()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void Save(User user)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
